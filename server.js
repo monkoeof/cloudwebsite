116a0d424f1bd870dfc6e1457fb3d05897945ab6
monkoeof <102807448+monkoeof@users.noreply.github.com>
2025-04-06 00:26:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,4 @@
-const express = require('express');
-const path = require('path');
-const app = express();
 const PORT = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname)));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
